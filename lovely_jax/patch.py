e3f28c76fc1dddcd3cab3b94be1e148ef7c341b0
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2023-01-24 19:03:39 +0700

    Skip sharded array tests

--- lovely_jax/patch.py
+++ lovely_jax/patch.py
@@ -16,7 +16,7 @@ from .repr_rgb import RGBProxy
 from .repr_plt import PlotProxy
 from .repr_chans import ChanProxy
 
-# %% ../nbs/10_patch.ipynb 8
+# %% ../nbs/10_patch.ipynb 6
 def _monkey_patch(cls):
     "Monkey-patch lovely features into `cls`" 
 
